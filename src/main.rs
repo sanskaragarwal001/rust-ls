917e4724a9df3ba3db54373163701c3e2a2bcd32
Sanskar Agarwal <aggrawalsanskar@gmail.com>
2026-04-19 14:15:02 +0530

    basic ls implemented successfully

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, Rust!");
+    /*
+     * ls [path] - without flags
+     */
+
+    let args: Vec<String> = env::args().collect();
+
+    let path = match args.last() {
+        Some(path) => path,
+        None => ".",
+    };
+
+    let contents = fs::read_dir(path).expect("failed to read the directory content.");
+    for content in contents {
+        match content {
+            Ok(meta) => println!("{0:?}", meta.file_name()),
+            Err(_) => eprintln!("failed to read the directory name."),
+        }
+    }
 }
