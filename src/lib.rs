2d38967480d2bc1ed0cf56c795a73355490b8cce
Sanskar Agarwal <aggrawalsanskar@gmail.com>
2026-04-19 22:51:09 +0530

    Update parser logic it now parses all of  flag

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,28 @@
 #[derive(Debug)]
 pub struct Config {
-    pub show_all: bool,
-    pub list_format: bool,
+    pub all: bool,
+    pub almost_all: bool,
+    pub print_reverse: bool,
+    pub show_subdirectories_content: bool,
+    pub newline: bool,
+    pub print_list_format: bool,
+    pub size: bool,
+    pub human_readable_size: bool,
     pub files: Vec<String>,
-    pub output: Option<String>,
 }
 
 impl Config {
     pub fn new() -> Self {
         Config {
-            show_all: false,
-            list_format: false,
+            all: false,
+            almost_all: false,
+            print_reverse: false,
+            show_subdirectories_content: false,
+            newline: false,
+            print_list_format: false,
+            size: false,
+            human_readable_size: false,
             files: vec![],
-            output: None,
         }
     }
-}
-
-pub fn show_all_files() {
-    println!("show all files");
-}
-
-pub fn show_list_format() {
-    println!("show list format");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,61 @@
-use std::env;
+use std::{env, process::exit};
 
 use rust_ls::Config;
 
+/*
+ * parser without
+ *
+ * - conflicting args like -a or -A
+ * - validation phase
+ * - error handling stratergy
+ * - add one value option (state machine)
+ */
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let mut config = Config::new();
-    let mut end_of_flags = false;
-    let mut expected_output = false;
-
     for arg in args.into_iter().skip(1) {
-        if expected_output {
-            if arg.starts_with("-") || arg.starts_with("--") {
-                eprintln!("expected file receive flag.");
-            } else {
-                config.output = Some(arg);
-            }
-            expected_output = false;
-            continue;
+        if arg.starts_with("---") {
+            exit(1);
         }
 
-        if arg == "--" {
-            end_of_flags = true;
-            continue;
-        }
-
-        if !end_of_flags {
-            // Lond Options
-            if arg.starts_with("--") {
-                match arg.as_str() {
-                    "--output" => expected_output = true,
-                    "--all" => config.show_all = true,
-                    "--list" => config.list_format = true,
-                    _ => eprintln!("Unknown option: {arg}"),
+        if arg.starts_with("--") {
+            match arg.as_str() {
+                "--all" => config.all = true,
+                "--almost-all" => config.almost_all = true,
+                "--human-readable" => config.human_readable_size = true,
+                "--reverse" => config.print_reverse = true,
+                "--recursive" => config.show_subdirectories_content = true,
+                _ => {
+                    eprintln!("Unknown flag {arg}");
+                    exit(1);
                 }
-                continue;
             }
-
-            // Short flags
-            if arg.starts_with("-") {
-                for ch in arg.chars().skip(1) {
-                    match ch {
-                        'a' => config.show_all = true,
-                        'l' => config.list_format = true,
-                        _ => eprintln!("Unknown flag: {arg}"),
+            continue;
+        } else if arg.starts_with("-") {
+            for ch in arg.chars().skip(1) {
+                match ch {
+                    'A' => config.almost_all = true,
+                    'a' => config.all = true,
+                    '1' => config.newline = true,
+                    'l' => config.print_list_format = true,
+                    'h' => config.human_readable_size = true,
+                    'r' => config.print_reverse = true,
+                    'R' => config.show_subdirectories_content = true,
+                    _ => {
+                        eprintln!("Unknown flag {ch}");
+                        exit(1);
                     }
                 }
-                continue;
             }
+            continue;
+        } else {
+            config.files.push(arg);
         }
-
-        config.files.push(arg);
     }
 
-    if expected_output {
-        eprintln!("Error: --output requires a value but none was provided");
+    if config.files.is_empty() {
+        config.files.push(String::from("."));
     }
-
-    println!("{:?}", config);
+    println!("{config:?}");
 }
